756c8f3353130f76d1af0e183a8e75b8ec8bb961
Akshay k <akshayk.info01@gmail.com>
2022-01-23 16:40:02 +0530

    ui for edit product

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route exact path="/" component={Home} />
       <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/dashboard/addProduct" component={AddProduct} />
+      <Route exact path="/dashboard/editProduct/:id" component={EditProduct} />
     </Switch>
   );
 }
