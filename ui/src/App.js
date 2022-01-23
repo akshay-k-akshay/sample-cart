f3331a372596042e7c5be123fa59b4a74e1c653a
Akshay k <akshayk.info01@gmail.com>
2022-01-23 14:02:00 +0530

    ui for list products to end user

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
+
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Switch>
+      <Route exact path="/" component={Home} />
 
-      </header>
-    </div>
+    </Switch>
   );
 }
 
